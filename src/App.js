57e592531a342364a18af704609b785a151665a6
sambabhouria <sambabhouria@gmail.com>
2019-12-01 23:41:32 +0100

    first commit tutorial hooks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./styles.css";
+import Header from "./components/header/header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/">
+            <Header />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+    //   <div className="App">
+    //     <Header />
+    //     <div className="Content">
+    //       <p>42 is the answer to everything...</p>
+    //     </div>
+    //   </div>
   );
 }
-
 export default App;
---
+++ src/components/header/header.js
@@ -0,0 +1,54 @@
+/* eslint-disable jsx-a11y/accessible-emoji */
+import React, { useState, useEffect } from "react";
+import logo from "../../assets/logo.svg";
+import "./header.css";
+import { CSSTransition } from "react-transition-group";
+
+export default function Header() {
+  const [isNavVisible, setNavVisibility] = useState(false);
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 700px)");
+    mediaQuery.addListener(handleMediaQueryChange);
+    handleMediaQueryChange(mediaQuery);
+
+    return () => {
+      mediaQuery.removeListener(handleMediaQueryChange);
+    };
+  }, []);
+
+  const handleMediaQueryChange = mediaQuery => {
+    if (mediaQuery.matches) {
+      setIsSmallScreen(true);
+    } else {
+      setIsSmallScreen(false);
+    }
+  };
+
+  const toggleNav = () => {
+    setNavVisibility(!isNavVisible);
+  };
+
+  return (
+    <header className="Header">
+      <img src={logo} className="Logo" alt="logo" />
+      <CSSTransition
+        in={!isSmallScreen || isNavVisible}
+        timeout={350}
+        classNames="NavAnimation"
+        unmountOnExit
+      >
+        <nav className="Nav">
+          <a href="/">Home</a>
+          <a href="/">Articles</a>
+          <a href="/">About</a>
+          <button>Logout</button>
+        </nav>
+      </CSSTransition>
+      <button onClick={toggleNav} className="Burger">
+        🍔
+      </button>
+    </header>
+  );
+}
