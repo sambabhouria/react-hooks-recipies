252ad6f990c35a42a6a49321ffbff8daa6f307ab
sambabhouria <sambabhouria@gmail.com>
2019-12-14 22:36:05 +0100

    rid-row-gap large to all the page

--- src/components/todo-list/usePersist.js
+++ src/components/todo-list/usePersist.js
@@ -5,7 +5,27 @@ export function usePersistedContext(context, key = "state") {
   return persistedContext ? JSON.parse(persistedContext) : context;
 }
 
+// useEffect(() => {
+//   const fetchBusinesses = () => {
+//      return fetch("theURL", {method: "GET"}
+//   )
+//     .then(res => normalizeResponseErrors(res))
+//     .then(res => {
+//       return res.json();
+//     })
+//     .then(rcvdBusinesses => {
+//       // some stuff
+//     })
+//     .catch(err => {
+//       // some error handling
+//     });
+// };
+// fetchBusinesses();
+// }, []);
+
 export function usePersistedReducer([state, dispatch], key = "state") {
-  useEffect(() => localStorage.setItem(key, JSON.stringify(state)), [state]);
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(state));
+  }, [state]);
   return [state, dispatch];
 }
